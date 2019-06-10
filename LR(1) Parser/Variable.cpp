7d44876efd2eb9189179df8a469828a98d722444
ziper02 <ziper02@gmail.com>
2019-06-11 01:07:26 +0300

    s

--- LR(1) Parser/Variable.cpp
+++ LR(1) Parser/Variable.cpp
@@ -5,7 +5,7 @@ using namespace std;
 Variable::Variable(string originalName)
 {
 	name = "";
-	this->name = originalName; //thrid
+	this->name = originalName; //thrsid
 	string temp;
 	for (int i = 0; i < originalName.size(); i++)
 	{
